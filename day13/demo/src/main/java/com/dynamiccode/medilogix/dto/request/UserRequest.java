f119c0582998543ddc956cc38f43f09ea723c49c
DIVYAKARTHIKAA <119106566+DIVYAKARTHIKAA@users.noreply.github.com>
2023-10-05 15:12:02 +0530

    Add files via upload

---
+++ day13/demo/src/main/java/com/dynamiccode/medilogix/dto/request/UserRequest.java
@@ -0,0 +1,24 @@
+package com.dynamiccode.medilogix.dto.request;
+
+import com.dynamiccode.medilogix.model.enu.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class UserRequest {
+    private String name;
+    private String email;
+    private String password;
+    private Boolean isEnabled;
+    private Role role;
+
+}
